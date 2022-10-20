5aafd1c835acd3e1f328d0396eac7194909daa95
Mleciu <91214003+Mleciu@users.noreply.github.com>
2022-10-20 21:18:39 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
+var hide = true
+var R
+var G
+var B
+
 function generate(){
 
-    var R = Math.floor(Math.random() * 256).toString(16);
-    var G = Math.floor(Math.random() * 256).toString(16);
-    var B = Math.floor(Math.random() * 256).toString(16);
-    $('#output').html("#" + R + G + B + "<br>" + R + " " + G + " " + B);
+    R = Math.floor(Math.random() * 256);
+    G = Math.floor(Math.random() * 256);
+    B = Math.floor(Math.random() * 256);
+
+    //I know that I could do this better
+    if(R <= 15){
+        R= "0" + R.toString(16);
+    }else{R = R.toString(16);}
+    if(G <= 15){
+        G = "0" + G.toString(16);
+    }else{G = G.toString(16);};
+    if(B <= 15){
+        B = "0" + B.toString(16);
+    }else{B = B.toString(16);}
+
+    $('#output').html("#" + R + G + B);
+    if(hide == false){
+        $('#preview').css("background-color", "#"+R+G+B)
+    }
+}
+function shide(){
+    if(hide == true){
+        hide = false
+        console.log(hide)
+        $('#preview').css("background-color", "#"+R+G+B)
+    }
+    else{
+        hide = true
+        $('#preview').css("background-color", "#222222")
+        console.log(hide)
+    }
 }
